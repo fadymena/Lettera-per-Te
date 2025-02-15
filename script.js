9696eab35e97abdc54734b40dcab647b76d8b8c5
fadymena <fadyadel.mena@studenti.unisalento.it>
2025-02-15 18:49:12 +0100

    Aggiunto mi manchi

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ function verificaLogin() {
 
 const testo =
   "Mia amata Ari, nemmeno l'intero mare basterebbe a placare la sete che ho di te. Sei la mia acqua, il mio respiro, il desiderio che non conosce fine. Più ti amo, più ti voglio... 💖";
+const testoMiManchi =
+  "Mi manchi come il sonno a uno studente sotto esami, come il sole nei giorni di cielo grigio, come un bagno caldo nel mare d'inverno... 💖";
 let i = 0;
+let j = 0;
 
 function scriviLettera() {
   if (i < testo.length) {
@@ -28,7 +31,22 @@ function scriviLettera() {
   }
 }
 
+function scriviLetteraMiManchi() {
+  if (j < testoMiManchi.length) {
+    document.getElementById("testoMiManchi").innerHTML +=
+      testoMiManchi.charAt(j);
+    j++;
+    setTimeout(scriviLetteraMiManchi, 50);
+  }
+}
+
 function mostraMessaggio() {
   document.getElementById("messaggioFinaleFiore").style.display = "block";
   document.getElementById("messaggioFinale").style.display = "block";
+  document.getElementById("testoMiManchi").style.display = "block";
+  scriviLetteraMiManchi();
+}
+
+function mostraMessaggioMiManchi() {
+  scriviLetteraMiManchi();
 }

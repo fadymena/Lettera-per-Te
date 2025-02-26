fd2f80af94037cf37f0f2b014ef175dd58643c09
fadymena <fadyadel.mena@studenti.unisalento.it>
2025-02-26 19:49:44 +0100

    new msg

--- script.js
+++ script.js
@@ -14,22 +14,31 @@ function verificaLogin() {
   }
 }
 
-const k_velocita_testo = 1;
+const k_velocita_testo = 0.8;
 
 const testo =
   "Mia amata Ari, nemmeno l'intero mare basterebbe a placare la sete che ho di te. Sei la mia acqua, il mio respiro, il desiderio che non conosce fine. Più ti amo, più ti voglio... 💖";
-const dataMiManchi = "15/02/2025 18:50";
-const testoMiManchi =
-  "Mi manchi come il sonno a uno studente sotto esami, come il sole nei giorni di cielo grigio, come un bagno caldo nel mare d'inverno... 💖";
-const dataMiManchi_2 = "22/02/2025 16:40";
-const testoMiManchi_2 =
-  "Ho il dubbio che leggerai mai questo messagggio, ma se lo dovessi mai fare sappi che ti penso tutto il tempo";
+
+const dataArray = [
+  {
+    '<p class="dataMessaggio">15/02/2025 18:50<p>': `<p class="contenutoMessaggio">
+      Mi manchi come il sonno a uno studente sotto esami, come il sole nei giorni di cielo grigio, come un bagno caldo nel mare d'inverno... 💖
+      </p>`,
+  },
+  {
+    '<img id="messaggioFinaleFiore" src="flower.gif" alt="my flower" /><p class="dataMessaggio">22/02/2025 16:40<p>': `
+    <p class="contenutoMessaggio">
+    Ho il dubbio che leggerai mai questo messagggio, ma se lo dovessi mai fare sappi che ti penso tutto il tempo
+    </p>`,
+  },
+  {
+    '<div id="heartChart"></div><p class="dataMessaggio">26/02/2025 19:10<p>': `<p class="contenutoMessaggio">
+      Ascolta questa canzone, è per te <a href="https://www.youtube.com/watch?v=-TXtyYZIiWc">Past Lives 🎸</a>
+    </p>`,
+  },
+];
 
 let i = 0;
-let j = 0;
-let k = 0;
-let l = 0;
-let m = 0;
 
 function scriviLettera() {
   if (i < testo.length) {
@@ -41,62 +50,21 @@ function scriviLettera() {
   }
 }
 
-function scriviLetteraMiManchi() {
-  if (k < dataMiManchi.length) {
-    document.getElementById("dataMiManchi").innerHTML += dataMiManchi.charAt(k);
-    k++;
-    setTimeout(scriviLetteraMiManchi, 50 * k_velocita_testo);
-  } else if (j < testoMiManchi.length) {
-    document.getElementById("testoMiManchi").innerHTML +=
-      testoMiManchi.charAt(j);
-    j++;
-    setTimeout(scriviLetteraMiManchi, 50 * k_velocita_testo);
-  } else if (l < dataMiManchi_2.length) {
-    document.getElementById("dataMiManchi_2").innerHTML +=
-      dataMiManchi_2.charAt(l);
-    l++;
-    setTimeout(scriviLetteraMiManchi, 50 * k_velocita_testo);
-  } else if (m < testoMiManchi_2.length) {
-    document.getElementById("testoMiManchi_2").innerHTML +=
-      testoMiManchi_2.charAt(m);
-    m++;
-    setTimeout(scriviLetteraMiManchi, 50 * k_velocita_testo);
-  } else {
-    document.getElementById("heartChart").style.display = "block";
-  }
-}
-
+const container = document.getElementById("output");
 function mostraMessaggio() {
-  document.getElementById("messaggioFinaleFiore").style.display = "block";
   document.getElementById("messaggioFinale").style.display = "block";
-  document.getElementById("testoMiManchi").style.display = "block";
-  document.getElementById("dataMiManchi").style.display = "block";
-  document.getElementById("dataMiManchi_2").style.display = "block";
-  document.getElementById("testoMiManchi_2").style.display = "block";
-  scriviLetteraMiManchi();
-  // drawHeart();
-}
+  container.style.display = "block";
+  scriviLettera();
 
-function mostraMessaggioMiManchi() {
-  scriviLetteraMiManchi();
-}
+ 
 
-const text_array = [
-  {
-    data: "15/02/2025 18:50",
-    text: "Mi manchi come il sonno a uno studente sotto esami, come il sole nei giorni di cielo grigio, come un bagno caldo nel mare d'inverno... 💖",
-  },
-  {
-    data: "22/02/2025 16:40",
-    text: "Ho il dubbio che leggerai mai questo messagggio, ma se lo dovessi mai fare sappi che ti penso tutto il tempo",
-  },
-];
-
-function mostraMessaggi() {
-  text_array.forEach((element, index) => {
+  dataArray.forEach((obj, index) => {
     setTimeout(() => {
-      document.getElementById("dataMiManchi").innerHTML = element.data;
-      document.getElementById("testoMiManchi").innerHTML = element.text;
-    }, 5000 * index);
+      Object.entries(obj).forEach(([date, message]) => {
+        const div = document.createElement("div");
+        div.innerHTML = `${date} ${message}`;
+        container.appendChild(div);
+      });
+    }, index * 500); // Ritardo di 1 secondo tra un messaggio e l'altro
   });
 }
